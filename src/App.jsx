040953c2170c6b9cbd56d03f4cee39195dd00a87
K Naresh <knareshbabu3602@gmail.com>
2025-01-01 10:58:35 +0530

    build header

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import NavBar from "./components/NavBar"
+function App() {
+  return (
+    <NavBar />
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+    return (
+        <header>
+            <nav>
+                <img src="src\assets\react-logo.png" alt="React Log"></img>
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
